9d2aab6478d38addb38b8b1c266c93f9e9e61a40
kaiovasa1999-ux <veselin.d.valkanov@gmail.com>
2023-01-08 09:34:46 +0200

    modify the validations require to be at least with 5 more then previous one

--- Aucion.sol
+++ Aucion.sol
@@ -2,21 +2,30 @@
 
 pragma solidity ^0.8.7;
 
-contract Aucion{
+contract Aucion {
     address public owner;
-    uint public startblock;
-    uint public endBlock;
+    uint256 public startblock;
+    uint256 public endBlock;
 
     bool public isAucionCanceld;
     address public highestBidder;
-    mapping(address => uint) public fundsPerBidder;
+    mapping(address => uint256) public fundsPerBidder;
 
-    event LogBid(address bidder,uint bid,address highestBidder,uint highestBid);
-    event LogWithdraw(address withdrawer,address withdrawlAccount, uint amount);
+    event LogBid(
+        address bidder,
+        uint256 bid,
+        address highestBidder,
+        uint256 highestBid
+    );
+    event LogWithdraw(
+        address withdrawer,
+        address withdrawlAccount,
+        uint256 amount
+    );
     event LogCancel();
 
-     constructor (uint _startBlock, uint _endBlock){
-        require(_startBlock <=_endBlock);
+    constructor(uint256 _startBlock, uint256 _endBlock) {
+        require(_startBlock <= _endBlock);
         require(_startBlock > block.number);
 
         owner = msg.sender;
@@ -24,88 +33,100 @@ contract Aucion{
         endBlock = _endBlock;
     }
 
-    modifier onlyAucrionIsOpen(){
-        require (isAucionCanceld == false, "auction is not open at the moment");
-        require ((block.number > startblock) && (block.number < endBlock));
+    modifier onlyAucrionIsOpen() {
+        require(isAucionCanceld == false, "auction is not open at the moment");
+        require((block.number > startblock) && (block.number < endBlock));
         _;
     }
 
-    modifier onlyOwner(){
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    modifier onlyNotOwner(){
+    modifier onlyNotOwner() {
         require(msg.sender != owner);
         _;
     }
 
-    modifier onlyAfterStart(){
-        require (block.number > startblock);
+    modifier onlyAfterStart() {
+        require(block.number > startblock);
         _;
     }
 
-    modifier onlyBeforeEnd(){
+    modifier onlyBeforeEnd() {
         require(block.number < endBlock);
         _;
     }
 
-    modifier onlyWhenAucionIsOpen(){
+    modifier onlyWhenAucionIsOpen() {
         require(isAucionCanceld == false);
         _;
     }
 
-    modifier EndOrCanceled(){
-        require (block.number > endBlock || isAucionCanceld == true);
+    modifier EndOrCanceled() {
+        require(block.number > endBlock || isAucionCanceld == true);
         _;
     }
 
-    function placeBid() public payable onlyAucrionIsOpen onlyNotOwner onlyAfterStart onlyBeforeEnd {
-        require (msg.value > 0);
+    function placeBid()
+        public
+        payable
+        onlyAucrionIsOpen
+        onlyNotOwner
+        onlyAfterStart
+        onlyBeforeEnd
+    {
+        require(msg.value > 0);
 
-        uint newBid = fundsPerBidder[msg.sender] + msg.value;
+        uint256 newBid = fundsPerBidder[msg.sender] + msg.value;
 
-        uint lastbiggestBid = fundsPerBidder[highestBidder];
+        uint256 lastbiggestBid = fundsPerBidder[highestBidder];
 
-        require(newBid > lastbiggestBid);
+        require(
+            newBid > lastbiggestBid + 5,
+            "you have to bid with 5 eth more then the biggest bid"
+        );
 
         fundsPerBidder[msg.sender] = newBid;
         highestBidder = msg.sender;
 
-        emit LogBid(msg.sender,newBid,highestBidder,lastbiggestBid);
+        emit LogBid(msg.sender, newBid, highestBidder, lastbiggestBid);
     }
 
     function withdraw() public EndOrCanceled {
         address payable withdrawAccount;
-        uint amountToWithdraw;
+        uint256 amountToWithdraw;
 
-        if(isAucionCanceld == true){
+        if (isAucionCanceld == true) {
             amountToWithdraw = fundsPerBidder[msg.sender];
-        }else{
+        } else {
             //check if the account is not of the highestBidder;
             require(msg.sender != highestBidder);
 
-            if(msg.sender == owner){
+            if (msg.sender == owner) {
                 amountToWithdraw = fundsPerBidder[highestBidder];
-            }
-            else{
+            } else {
                 amountToWithdraw = fundsPerBidder[msg.sender];
             }
         }
 
         require(amountToWithdraw > 0);
 
-
         fundsPerBidder[withdrawAccount] -= amountToWithdraw;
 
-        msg.sender.transfer(amountToWithdraw);
+        payable(msg.sender).transfer(amountToWithdraw);
 
-        emit LogWithdraw(msg.sender,withdrawAccount,amountToWithdraw);
+        emit LogWithdraw(msg.sender, withdrawAccount, amountToWithdraw);
     }
 
-    function cancelAucion() public  onlyOwner onlyBeforeEnd onlyWhenAucionIsOpen {
-        isAucionCanceld =true;
+    function cancelAucion()
+        public
+        onlyOwner
+        onlyBeforeEnd
+        onlyWhenAucionIsOpen
+    {
+        isAucionCanceld = true;
         emit LogCancel();
     }
-    
 }
